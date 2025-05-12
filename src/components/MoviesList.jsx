8191cf45f167c8b20609f1f5599b5e2af9337d9e
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-12 15:00:58 +0200

    feat: create movies list, using axios to get info from my api

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function MoviesList() {
+
+    const [movies, setMovies] = useState([]);
+
+    const url = 'http://127.0.0.1:3000';
+
+    function getMovies() {
+        axios.get(`${url}/movies`)
+            .then(res => {
+                console.log(res.data);
+                setMovies(res.data);
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(getMovies, []);
+
     return (<>
         <h2>Lista dei film:</h2>
         <ul>
-            <li>Film</li>
+            {movies.map(movie => <li key={movie.id}>{movie.title}</li>)}
         </ul>
     </>
     )
