a2efa4f1221293d553589399d3d25d94163135ad
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-12 16:21:02 +0200

    feat: create search fn

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -5,22 +5,40 @@ import MovieCard from "./MovieCard";
 function MoviesList() {
 
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('');
 
     const url = 'http://127.0.0.1:3000';
 
     function getMovies() {
-        axios.get(`${url}/movies`)
+        axios.get(`${url}/movies`, {
+            params: { search }
+        })
             .then(res => {
-                // console.log(res.data);
                 setMovies(res.data);
             })
             .catch(err => console.log(err))
     }
 
-    useEffect(getMovies, []);
+    function searchMovies(event) {
+        event.preventDefault();
+
+        getMovies();
+    }
+
+    useEffect(getMovies, [search]);
 
     return (<>
         <h2>Lista dei film:</h2>
+        <form onSubmit={searchMovies}>
+            <input
+                type="text"
+                placeholder="Cerca..."
+                value={search}
+                onChange={(event) => setSearch(event.target.value)}
+            />
+            <button type="submit">Cerca</button>
+        </form>
+
         <ul>
             {movies.length ? movies.map(movie => <MovieCard key={movie.id} data={movie} />) : <div>Il film non è presente!</div>}
         </ul>
