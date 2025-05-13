bed1ab1d2db57b028bc40bf3a21175e0f1514da3
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-13 14:55:55 +0200

    feat: create instructions to represent all the reviews of a movie

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -7,10 +7,15 @@ function MovieDetail() {
     const url = 'http://127.0.0.1:3000';
 
     const [movie, setMovie] = useState({});
+    const [review, setReview] = useState([]);
 
     function getMovie() {
         axios.get(`${url}/movies/${id}`)
-            .then(res => setMovie(res.data))
+            .then(res => {
+                setMovie(res.data);
+                console.log(res.data.reviews);
+                setReview(res.data.reviews);
+            })
             .catch(err => console.error(err))
     }
 
@@ -24,11 +29,21 @@ function MovieDetail() {
             <p>Description: <em>{movie.abstract}</em></p>
         </div>
 
-        <h3>
-            Our reviews:
-        </h3>
         <div>
-            <p>Vote: {movie.mean_votes}</p>
+            <h3>Our reviews:</h3>
+            <div>
+                <p>Vote: {movie.mean_votes}</p>
+            </div>
+
+            {review.length ? review.map(rev =>
+                <div key={rev.id} className="card mb-4">
+                    <div className="card-body">
+                        <h4>{rev.name}</h4>
+                        <p>{rev.text}</p>
+                        <p><em>Vote:</em> {rev.vote}</p>
+                    </div>
+                </div>) : <p>Nessuna recensione</p>}
+
         </div>
     </>
 }
