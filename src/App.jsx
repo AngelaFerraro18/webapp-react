0604d750f016fa7b72badcd31e15e340dfc67fe0
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-15 18:10:46 +0200

    feat: create global context in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,30 @@ import MoviesList from "./pages/MoviesList"
 import DefaultLayout from "./layouts/DefaultLayout"
 import MovieDetail from "./pages/MovieDetail"
 import AddMovieCard from "./pages/AddMovieCard"
+import GlobalContext from "./contexts/globalContext"
+import { useState } from "react"
 
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<MoviesList />} />
-            <Route path="/movies/:id" element={<MovieDetail />} />
-            <Route path="/movies/new-movie-card" element={<AddMovieCard />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalContext.Provider value={{
+        isLoading,
+        setIsLoading
+      }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<MoviesList />} />
+              <Route path="/movies/:id" element={<MovieDetail />} />
+              <Route path="/movies/new-movie-card" element={<AddMovieCard />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
