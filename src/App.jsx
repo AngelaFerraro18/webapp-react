7650fd87bf551c316ddd536fd4673e288d4f7684
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-15 17:01:46 +0200

    feat: create a new route to page to add a movie

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import MoviesList from "./pages/MoviesList"
 import DefaultLayout from "./layouts/DefaultLayout"
 import MovieDetail from "./pages/MovieDetail"
+import AddMovieCard from "./pages/AddMovieCard"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route element={<DefaultLayout />}>
             <Route path="/" element={<MoviesList />} />
             <Route path="/movies/:id" element={<MovieDetail />} />
+            <Route path="/movies/new-movie-card" element={<AddMovieCard />} />
           </Route>
         </Routes>
       </BrowserRouter>
