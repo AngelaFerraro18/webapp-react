b7d2cfb7d61b8e4396e9327379525ebacec32951
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-15 17:24:06 +0200

    feat: add a btn to reach AddMovieCard page

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
+import { Link } from "react-router-dom";
 
 function MoviesList() {
 
@@ -29,18 +30,22 @@ function MoviesList() {
 
     return (<>
         <h2 className="mb-2">Lista dei film:</h2>
-        <form onSubmit={searchMovies} className="mb-5 col-3">
-            <div className="input-group">
-                <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Cerca..."
-                    value={search}
-                    onChange={(event) => setSearch(event.target.value)}
-                />
-                <button className="btn btn-primary" type="submit">Cerca</button>
-            </div>
-        </form>
+        <div className="d-flex justify-content-between align-items-center">
+            <form onSubmit={searchMovies} className="mb-5 col-3">
+                <div className="input-group">
+                    <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Cerca..."
+                        value={search}
+                        onChange={(event) => setSearch(event.target.value)}
+                    />
+                    <button className="btn btn-primary" type="submit">Cerca</button>
+                </div>
+            </form>
+            <Link to={'/movies/new-movie-card'} className="btn btn-primary mb-5">Add a new movie!</Link>
+        </div>
+
 
         <ul className="d-flex gap-3 row justify-content-center mb-5">
             {movies.length ? movies.map(movie => <MovieCard key={movie.id} data={movie} />) : <div>Il film non è presente!</div>}
